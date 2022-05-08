46c3a64e858480551ac7f0091c81946f197a8d4d
Vinicius Caetano <viniciusbeschcaetano@gmail.com>
2022-05-08 19:50:39 -0300

    Erro: this não tem referência

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import SearchBar from './SearchBar'
 import axios from 'axios';
+import SearchBar from './SearchBar';
 
 class App extends React.Component {
+    state = { images: [] };
+
     async onSearchSubmit(term) {
         const response = await axios.get('https://api.unsplash.com/search/photos', {
             params: { query: term },
             headers: {
-                Authorization: 'Client-ID ImZ6FQkII0CtazperPTEYK0Gv9w4QSd7RmyKnU_Frus'
-            }
+                Authorization:
+                    'Client-ID ImZ6FQkII0CtazperPTEYK0Gv9w4QSd7RmyKnU_Frus',
+            },
         });
-        console.log(response.data.results);
+
+        console.log(this);
+        this.setState({ images: response.data.results });
     }
 
     render() {
         return (
             <div className="ui container" style={{ marginTop: '10px' }}>
-                <SearchBar onSubmit={this.onSearchSubmit} />
+                <SearchBar
+                    onSubmit={this.onSearchSubmit}
+                    guesswhtIam="Im the propsobject"
+                />
+                Found: {this.state.images.length} images
             </div>
-        )
+        );
     }
 }
 
