18251e9a509e13166ea81f687c436c04e4548c04
Vinicius Caetano <viniciusbeschcaetano@gmail.com>
2022-05-08 18:57:09 -0300

    Refatora para controlled forms

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,20 +1,18 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-    onInputChange(event) {
-        console.log(event.target.value);
-    }
 
-    onInputClick(event) {
-        console.log("User clicked")
-    }
+    state = { term: '' };
 
     render() {
         return (<div className="ui segment">
             <form className="ui form">
                 <div className="field">
                     <label>Search images</label>
-                    <input type="text" onChange={this.onInputChange} onClick={this.onInputClick} />
+                    <input
+                        type="text"
+                        value={this.state.term}
+                        onChange={(e) => this.setState({ term: e.target.value })} />
                 </div>
             </form>
         </div>
