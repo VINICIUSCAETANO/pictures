cd9096a9cf707751876b1a5db2963388c186641a
Vinicius Caetano <viniciusbeschcaetano@gmail.com>
2022-05-08 18:49:25 -0300

    Acrescenta métodos onInputChange e onInputClick

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    onInputChange(event) {
+        console.log(event.target.value);
+    }
+
+    onInputClick(event) {
+        console.log("User clicked")
+    }
+
     render() {
         return (<div className="ui segment">
             <form className="ui form">
                 <div className="field">
-                    <input type="text" />
+                    <label>Search images</label>
+                    <input type="text" onChange={this.onInputChange} onClick={this.onInputClick} />
                 </div>
             </form>
         </div>
